dcefcd362416126c896cbae4cae9a8b23f9d6be1
The Sanskari Ladka <69839639+THESANSKARILADKA@users.noreply.github.com>
2020-09-14 08:33:15 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -850,7 +850,7 @@ async def vapor(vpr):
 @register(outgoing=True, pattern="^.repo$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Click [here](https://github.com/DARK-COBRA/DARKCOBRA) to open this cool userbot repo 𝔻𝔸ℝ𝕂 ℂ𝕆𝔹ℝ𝔸.")
+        await e.edit("Click [here](https://github.com/THESANSKARILADKA/SANSKARI-USERBOT) to open this cool userbot repo names as ᔑᗩᑎᔑᏦᗩᖇᏆ ᑌᔑᗴᖇᗷᝪᎢ.")
 			  
 			  
 @register(outgoing=True, pattern="^.str(?: |$)(.*)")
